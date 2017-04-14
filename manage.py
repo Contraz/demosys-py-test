ed668469272031626cfb89d04ea11c0e29373abd
Einar Forselv <eforselv@gmail.com>
2017-04-14 21:24:28 +0200

    Bump version

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DEMOSYS_SETTINGS_MODULE", "demosys_test.settings")
+    os.environ.setdefault("DEMOSYS_SETTINGS_MODULE", "testdemo.settings")
 
     from demosys.core.management import execute_from_command_line
 
