3afb61023d90f41f6a159564338824c12bc5ad92
Einar Forselv <eforselv@gmail.com>
2018-06-24 20:21:32 +0200

    Convert transform feedback effect

--- testdemo/feedback/effect.py
+++ testdemo/feedback/effect.py
@@ -1,13 +1,16 @@
-from demosys.effects import effect
-from demosys.opengl import geometry
-from OpenGL import GL
-from OpenGL.arrays.vbo import VBO
-from demosys.opengl import VAO
+import moderngl as mgl
+import math
 import numpy
 import random
+
+from OpenGL import GL
+
+from demosys import geometry
+from demosys.effects import effect
+from demosys.opengl import VAO
+
 from pyrr import matrix44
 from pyrr import vector3
-import math
 
 
 class DefaultEffect(effect.Effect):
@@ -16,12 +19,15 @@ class DefaultEffect(effect.Effect):
         self.feedback = self.get_shader("feedback/feedback.glsl")
         self.shader = self.get_shader("feedback/default.glsl")
         self.texture = self.get_texture("feedback/particle.png")
+
         self.cube = geometry.cube(width=4, height=4, depth=4)
         self.cube_shader = self.get_shader("feedback/cube.glsl")
+
         # VAOs representing the two different buffer bindings
         self.particles1 = None
         self.particles2 = None
         self.particles = None
+
         # VBOs for each position buffer
         self.pos1 = None
         self.pos2 = None
@@ -34,9 +40,8 @@ class DefaultEffect(effect.Effect):
 
         GL.glEnable(GL.GL_BLEND)
         GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
-        # GL.glBlendFunc(GL.GL_ONE, GL.GL_ONE)
 
-        m_proj = self.create_projection(90, 1, 1000)
+        m_proj = self.create_projection(90.0, 1.0, 1000.0)
 
         # Rotate and translate
         m_mv = self.create_transformation(rotation=(time * 0.0, time * 0, time * 0),
@@ -51,54 +56,27 @@ class DefaultEffect(effect.Effect):
                                      math.sin(time/3) * 5)
         gravity_force = math.cos(time / 2) * 3.0 + 3.0
         # gravity_force = 2.0
-        # Transform
-        with self.particles.bind(self.feedback) as s:
-            s.uniform_3fv("gravity_pos", gravity_pos)
-            s.uniform_1f("gravity_force", gravity_force)
-            s.uniform_1f("timedelta", frametime)
 
-        GL.glEnable(GL.GL_RASTERIZER_DISCARD)
-        GL.glBindBufferBase(GL.GL_TRANSFORM_FEEDBACK_BUFFER, 0, self.pos)
-        GL.glBeginTransformFeedback(GL.GL_POINTS)
-        self.particles.draw()
-        GL.glEndTransformFeedback()
-        GL.glDisable(GL.GL_RASTERIZER_DISCARD)
+        # Transform
+        self.feedback.uniform("gravity_pos", gravity_pos.astype('f4').tobytes())
+        self.feedback.uniform("gravity_force", gravity_force)
+        self.feedback.uniform("timedelta", frametime)
+        self.particles.transform(self.feedback, self.pos)
 
         # Draw particles
-        with self.particles.bind(self.shader) as shader:
-            # shader.uniform_mat4("m_proj", self.sys_camera.projection)
-            shader.uniform_mat4("m_proj", m_proj)
-            shader.uniform_mat4("m_mv", m_mv)
-            shader.uniform_sampler_2d(0, "texture0", self.texture)
-        self.particles.draw()
-
-        # # Rotate and translate
-        # m_mv = self.create_transformation(rotation=(time * 0.02 * gravity_force,
-        #                                             time * 0.01 * gravity_force,
-        #                                             time * 0.025 * gravity_force),
-        #                                   translation=(0.0, 0.0, 0.0))
-        #
-        # # Apply the rotation and translation from the system camera
-        # m_mv = matrix44.multiply(m_mv, self.sys_camera.view_matrix)
-        #
-        # # Create normal matrix from model-view
-        # m_normal = self.create_normal_matrix(m_mv)
-        #
-        # GL.glDisable(GL.GL_BLEND)
-        # GL.glEnable(GL.GL_DEPTH_TEST)
-        # with self.cube.bind(self.cube_shader) as s:
-        #     s.uniform_mat4("m_proj", m_proj)
-        #     s.uniform_mat4("m_mv", m_mv)
-        #     s.uniform_mat3("m_normal", m_normal)
-        # self.cube.draw()
+        self.shader.uniform("m_proj", m_proj.astype('f4').tobytes())
+        self.shader.uniform("m_mv", m_mv.astype('f4').tobytes())
+        self.texture.use(location=0)
+        self.shader.uniform("texture0", 0)
+        self.particles.draw(self.shader)
 
         # Swap buffers
         self.pos = self.pos1 if self.pos == self.pos2 else self.pos2
         self.particles = self.particles1 if self.particles == self.particles2 else self.particles2
 
     def init_particles(self):
-        count = 100000
-        area = 30.0
+        count = 10000
+        area = 60.0
         speed = 5.0
 
         def gen():
@@ -116,22 +94,14 @@ class DefaultEffect(effect.Effect):
 
         data1 = numpy.fromiter(gen(), count=count * 6, dtype=numpy.float32)
         data2 = numpy.fromiter(gen(), count=count * 6, dtype=numpy.float32)
-        # data1 = numpy.array(d, dtype=numpy.float32)
-        # data2 = numpy.array(d, dtype=numpy.float32)
 
-        self.pos1 = VBO(data1)
-        self.particles1 = VAO("particles1", mode=GL.GL_POINTS)
-        self.particles1.add_array_buffer(GL.GL_FLOAT, self.pos1)
-        self.particles1.map_buffer(self.pos1, "in_position", 3)
-        self.particles1.map_buffer(self.pos1, "in_velocity", 3)
-        self.particles1.build()
+        self.pos1 = self.ctx.buffer(data1.tobytes())
+        self.particles1 = VAO("particles1", mode=mgl.POINTS)
+        self.particles1.buffer(self.pos1, '3f 3f', ['in_position', 'in_velocity'])
 
-        self.pos2 = VBO(data2)
-        self.particles2 = VAO("particles2", mode=GL.GL_POINTS)
-        self.particles2.add_array_buffer(GL.GL_FLOAT, self.pos2)
-        self.particles2.map_buffer(self.pos2, "in_position", 3)
-        self.particles2.map_buffer(self.pos2, "in_velocity", 3)
-        self.particles2.build()
+        self.pos2 = self.ctx.buffer(data2.tobytes())
+        self.particles2 = VAO("particles2", mode=mgl.POINTS)
+        self.particles2.buffer(self.pos2, '3f 3f', ['in_position', 'in_velocity'])
 
         # Set initial start buffers
         self.particles = self.particles1
